5c5d8a379180b4cdb00a8864be06584ea5f0e3c4
Am-Awal <am.awal@outlook.com>
2022-08-27 11:49:17 -0500

    Initial Commit

---
+++ DAofAlgorithms/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DAofAlgorithms
+//
+//  Created by Awal Amadou on 8/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DAofAlgorithms/DAofAlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DAofAlgorithmsApp.swift
+//  DAofAlgorithms
+//
+//  Created by Awal Amadou on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct DAofAlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
